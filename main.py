f8e97fe7e0c5d286fc6891174a4b330d7ad1e375
Aditi <adisin0427@gmail.com>
2026-04-08 21:34:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+from fastapi import FastAPI
+from rag import load_db, get_answer
+
+
+app = FastAPI()
+
+db = load_db()
+
+@app.get("/")
+def home():
+    return {"message": "Placement Prep Copilot Running 🚀"}
+
+@app.get("/ask")
+def ask(query: str):
+    answer = get_answer(query, db)
+    return {
+        "question": query,
+        "answer": answer
+    }
